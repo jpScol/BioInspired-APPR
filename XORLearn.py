a0a057d59d6a8dfacb112322f8e2963df91e36f4
Bruju <bruyat.ju@live.fr>
2020-01-05 18:11:56 +0100

    XORLearn

--- XORLearn.py
+++ XORLearn.py
@@ -1,6 +1,10 @@
 import torch
-import torch.nn as nn
+import random
 
+# This files shows how to build a neural network using pytorch to learn the
+# XOR operation
+
+# Function to learn
 def xor(a, b):
     if a == 1 and b == 1:
         return 0
@@ -9,24 +13,27 @@ def xor(a, b):
     else:
         return 1
 
-model = nn.Sequential(
-    nn.Linear(2, 10),
-    nn.ReLU(),
-    nn.Linear(10, 1)
+# Torch model
+model = torch.nn.Sequential(
+    torch.nn.Linear(2, 10),
+    torch.nn.ReLU(),
+    torch.nn.Linear(10, 1)
 )
 
+# Parameters
 loss_fn = torch.nn.MSELoss()
 learning_rate = 1e-2
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
-import random
-import numpy
-
 score = 0
 
 steps = 1000
+params = {
+    'steps': 1000,
+    'evaluated_steps': 250
+}
 
-for step in range(steps):
+for step in range(params['steps']):
     a = random.randint(0, 1)
     b = random.randint(0, 1)
     x = torch.Tensor([a, b])
@@ -34,22 +41,18 @@ for step in range(steps):
     y_pred = model(x)
     y = xor(a, b)
     
-    yp = torch.Tensor([1])
-    yp[0] = y
+    yp = torch.Tensor([y])
     loss = loss_fn(y_pred, yp)
 
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
 
-    if step > steps - 250:
+    # Evaluation
+    if step >= params['steps'] - params['evaluated_steps']:
         if y == 0 and y_pred.item() < 0.5:
             score = score + 1
         if y == 1 and y_pred.item() >= 0.5:
             score = score + 1
 
-        print(x)
-        print(y_pred)
-        print(y)
-
-print(score)
+print("Score : " + str(score) + " / " + str(params['evaluated_steps']))
